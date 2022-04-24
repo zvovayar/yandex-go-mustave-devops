279fb2012559075e9f944088e27c9e15fd445659
zvovayar <zvovayar@gmail.com>
2022-04-24 21:27:41 +0300

    internal/datatypes added

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,68 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
-	"net/http"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 	"time"
+
+	"github.com/zvovayar/yandex-go-mustave-devops/internal"
 )
 
-type gauge float64
-type counter int64
-type monitor struct {
-	// warning: remember about quantity metrics!
-	Gmetrics [28]gauge
-	Cmetrics [1]counter
-}
-
-var gmetricnames = map[string]int{
-	"Alloc":         0,
-	"BuckHashSys":   1,
-	"Frees":         2,
-	"GCCPUFraction": 3,
-	"GCSys":         4,
-	"HeapAlloc":     5,
-	"HeapIdle":      6,
-	"HeapInuse":     7,
-	"HeapObjects":   8,
-	"HeapReleased":  9,
-	"HeapSys":       10,
-	"LastGC":        11,
-	"Lookups":       12,
-	"MCacheInuse":   13,
-	"MCacheSys":     14,
-	"MSpanInuse":    15,
-	"MSpanSys":      16,
-	"Mallocs":       17,
-	"NextGC":        18,
-	"NumForcedGC":   19,
-	"NumGC":         20,
-	"OtherSys":      21,
-	"PauseTotalNs":  22,
-	"StackInuse":    23,
-	"StackSys":      24,
-	"Sys":           25,
-	"TotalAlloc":    26,
-	"RandomValue":   27,
-}
-var cmetricnames = map[string]int{
-	"PollCount": 0,
-}
-var pollInterval = time.Second * 2    // 2
-var reportInterval = time.Second * 10 //10
-var bufferLength = 20
-var serverAddress = "127.0.0.1:8080"
-var contentType = "text/plain"
-
-func NewMonitor(duration time.Duration, chanmonitor chan monitor) {
-	var m monitor
+func NewMonitor(duration time.Duration, chanmonitor chan internal.Monitor) {
+	var m internal.Monitor
 	var rtm runtime.MemStats
 
 	for {
@@ -72,40 +25,40 @@ func NewMonitor(duration time.Duration, chanmonitor chan monitor) {
 		runtime.ReadMemStats(&rtm)
 
 		// Collect stats
-		m.Gmetrics[gmetricnames["Alloc"]] = gauge(rtm.Alloc)
-		m.Gmetrics[gmetricnames["BuckHashSys"]] = gauge(rtm.BuckHashSys)
-		m.Gmetrics[gmetricnames["Frees"]] = gauge(rtm.Frees)
-		m.Gmetrics[gmetricnames["GCCPUFraction"]] = gauge(rtm.GCCPUFraction)
-		m.Gmetrics[gmetricnames["GCSys"]] = gauge(rtm.GCSys)
-		m.Gmetrics[gmetricnames["HeapAlloc"]] = gauge(rtm.HeapAlloc)
-		m.Gmetrics[gmetricnames["HeapIdle"]] = gauge(rtm.HeapIdle)
-		m.Gmetrics[gmetricnames["HeapInuse"]] = gauge(rtm.HeapInuse)
-		m.Gmetrics[gmetricnames["HeapObjects"]] = gauge(rtm.HeapObjects)
-		m.Gmetrics[gmetricnames["HeapReleased"]] = gauge(rtm.HeapReleased)
-		m.Gmetrics[gmetricnames["HeapSys"]] = gauge(rtm.HeapSys)
-		m.Gmetrics[gmetricnames["LastGC"]] = gauge(rtm.LastGC)
-		m.Gmetrics[gmetricnames["Lookups"]] = gauge(rtm.Lookups)
-		m.Gmetrics[gmetricnames["MCacheInuse"]] = gauge(rtm.MCacheInuse)
-		m.Gmetrics[gmetricnames["MCacheSys"]] = gauge(rtm.MCacheSys)
-		m.Gmetrics[gmetricnames["MSpanInuse"]] = gauge(rtm.MSpanInuse)
-		m.Gmetrics[gmetricnames["MSpanSys"]] = gauge(rtm.MSpanSys)
-		m.Gmetrics[gmetricnames["Mallocs"]] = gauge(rtm.Mallocs)
-		m.Gmetrics[gmetricnames["NextGC"]] = gauge(rtm.NextGC)
-		m.Gmetrics[gmetricnames["NumForcedGC"]] = gauge(rtm.NumForcedGC)
-		m.Gmetrics[gmetricnames["NumGC"]] = gauge(rtm.NumGC)
-		m.Gmetrics[gmetricnames["OtherSys"]] = gauge(rtm.OtherSys)
-		m.Gmetrics[gmetricnames["PauseTotalNs"]] = gauge(rtm.PauseTotalNs)
-		m.Gmetrics[gmetricnames["StackInuse"]] = gauge(rtm.StackInuse)
-		m.Gmetrics[gmetricnames["StackSys"]] = gauge(rtm.StackSys)
-		m.Gmetrics[gmetricnames["Sys"]] = gauge(rtm.Sys)
-		m.Gmetrics[gmetricnames["TotalAlloc"]] = gauge(rtm.TotalAlloc)
-		m.Gmetrics[gmetricnames["RandomValue"]] = gauge(rand.Float64())
+		m.Gmetrics[internal.Gmetricnames["Alloc"]] = internal.Gauge(rtm.Alloc)
+		m.Gmetrics[internal.Gmetricnames["BuckHashSys"]] = internal.Gauge(rtm.BuckHashSys)
+		m.Gmetrics[internal.Gmetricnames["Frees"]] = internal.Gauge(rtm.Frees)
+		m.Gmetrics[internal.Gmetricnames["GCCPUFraction"]] = internal.Gauge(rtm.GCCPUFraction)
+		m.Gmetrics[internal.Gmetricnames["GCSys"]] = internal.Gauge(rtm.GCSys)
+		m.Gmetrics[internal.Gmetricnames["HeapAlloc"]] = internal.Gauge(rtm.HeapAlloc)
+		m.Gmetrics[internal.Gmetricnames["HeapIdle"]] = internal.Gauge(rtm.HeapIdle)
+		m.Gmetrics[internal.Gmetricnames["HeapInuse"]] = internal.Gauge(rtm.HeapInuse)
+		m.Gmetrics[internal.Gmetricnames["HeapObjects"]] = internal.Gauge(rtm.HeapObjects)
+		m.Gmetrics[internal.Gmetricnames["HeapReleased"]] = internal.Gauge(rtm.HeapReleased)
+		m.Gmetrics[internal.Gmetricnames["HeapSys"]] = internal.Gauge(rtm.HeapSys)
+		m.Gmetrics[internal.Gmetricnames["LastGC"]] = internal.Gauge(rtm.LastGC)
+		m.Gmetrics[internal.Gmetricnames["Lookups"]] = internal.Gauge(rtm.Lookups)
+		m.Gmetrics[internal.Gmetricnames["MCacheInuse"]] = internal.Gauge(rtm.MCacheInuse)
+		m.Gmetrics[internal.Gmetricnames["MCacheSys"]] = internal.Gauge(rtm.MCacheSys)
+		m.Gmetrics[internal.Gmetricnames["MSpanInuse"]] = internal.Gauge(rtm.MSpanInuse)
+		m.Gmetrics[internal.Gmetricnames["MSpanSys"]] = internal.Gauge(rtm.MSpanSys)
+		m.Gmetrics[internal.Gmetricnames["Mallocs"]] = internal.Gauge(rtm.Mallocs)
+		m.Gmetrics[internal.Gmetricnames["NextGC"]] = internal.Gauge(rtm.NextGC)
+		m.Gmetrics[internal.Gmetricnames["NumForcedGC"]] = internal.Gauge(rtm.NumForcedGC)
+		m.Gmetrics[internal.Gmetricnames["NumGC"]] = internal.Gauge(rtm.NumGC)
+		m.Gmetrics[internal.Gmetricnames["OtherSys"]] = internal.Gauge(rtm.OtherSys)
+		m.Gmetrics[internal.Gmetricnames["PauseTotalNs"]] = internal.Gauge(rtm.PauseTotalNs)
+		m.Gmetrics[internal.Gmetricnames["StackInuse"]] = internal.Gauge(rtm.StackInuse)
+		m.Gmetrics[internal.Gmetricnames["StackSys"]] = internal.Gauge(rtm.StackSys)
+		m.Gmetrics[internal.Gmetricnames["Sys"]] = internal.Gauge(rtm.Sys)
+		m.Gmetrics[internal.Gmetricnames["TotalAlloc"]] = internal.Gauge(rtm.TotalAlloc)
+		m.Gmetrics[internal.Gmetricnames["RandomValue"]] = internal.Gauge(rand.Float64())
 
-		m.Cmetrics[cmetricnames["PoolCount"]]++
+		m.Cmetrics[internal.Cmetricnames["PoolCount"]]++
 
 		// Just encode to json and print
 		b, _ := json.Marshal(m)
-		log.Println("NewMonitor - > " + string(b))
+		log.Printf("NewMonitor - > %v Channel length %v", string(b), len(chanmonitor))
 
 		// Save new collected data to the slice
 		chanmonitor <- m
@@ -116,19 +69,19 @@ func NewMonitor(duration time.Duration, chanmonitor chan monitor) {
 }
 
 func main() {
-	chanmonitor := make(chan monitor, bufferLength)
+	chanmonitor := make(chan internal.Monitor, internal.BufferLength)
 	chanOS := make(chan os.Signal, 1) // we need to reserve to buffer size 1, so the notifier are not blocked
 	signal.Notify(chanOS, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
 
-	go NewMonitor(pollInterval, chanmonitor)
-	go runSendMetrics(reportInterval, chanmonitor)
+	go NewMonitor(internal.PollInterval, chanmonitor)
+	go runSendMetrics(internal.ReportInterval, chanmonitor)
 
 	sig := <-chanOS
 	log.Printf("INFO got a signal '%v', start shutting down...\n", sig) // put breakpoint here
 	log.Printf("Shutdown complete")
 }
 
-func runSendMetrics(duration time.Duration, chanmonitor chan monitor) {
+func runSendMetrics(duration time.Duration, chanmonitor chan internal.Monitor) {
 
 	for {
 		<-time.After(duration)
@@ -146,63 +99,5 @@ func runSendMetrics(duration time.Duration, chanmonitor chan monitor) {
 
 		}
 
-	}
-}
-
-func (m monitor) SendMetrics() {
-
-	//Just encode to json and print
-	b, _ := json.Marshal(m)
-	log.Println("SendMetrics -> " + string(b))
-	var body = []byte(b)
-
-	// gauge type send
-	for key, element := range gmetricnames {
-		var url = "http://" + serverAddress + "/update/gauge/" + key + "/" + fmt.Sprint(m.Gmetrics[element])
-		log.Println(url)
-
-		request, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-		if err != nil {
-			// обработаем ошибку
-			log.Println(err)
-		}
-		request.Header.Set("Content-Type", contentType)
-
-		client := &http.Client{}
-		// отправляем запрос
-
-		resp, err := client.Do(request)
-		if err != nil {
-			// обработаем ошибку
-			log.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-		log.Println(resp)
-	}
-
-	// counter type send
-	for key, element := range cmetricnames {
-		var url = "http://" + serverAddress + "/update/counter/" + key + "/" + fmt.Sprint(m.Cmetrics[element])
-		log.Println(url)
-
-		request, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-		if err != nil {
-			// обработаем ошибку
-			log.Println(err)
-		}
-		request.Header.Set("Content-Type", contentType)
-
-		client := &http.Client{}
-		// отправляем запрос
-
-		resp, err := client.Do(request)
-		if err != nil {
-			// обработаем ошибку
-			log.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-		log.Println(resp)
 	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	// маршрутизация запросов обработчику
+
+	// root
+	http.HandleFunc("/", HelloWorld)
+	// update
+	http.HandleFunc("/update/", UpdateMetric)
+
+	// запуск сервера с адресом localhost, порт 8080
+	http.ListenAndServe(":8080", nil)
+}
+
+// HelloWorld — обработчик запроса.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+
+}
+
+// HelloWorld — обработчик запроса.
+func UpdateMetric(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+
+}
