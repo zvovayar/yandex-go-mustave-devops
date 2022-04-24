697333ae9e0c5d8bf4f21857e87fd969649cffdf
zvovayar <zvovayar@gmail.com>
2022-04-24 22:57:24 +0300

    two handlers gauge and counters

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,15 +10,23 @@ func main() {
 	// root
 	http.HandleFunc("/", http.NotFound)
 	// update
-	http.HandleFunc("/update/", UpdateMetric)
+	http.HandleFunc("/update/gauge/", UpdateGaugeMetric)
+	http.HandleFunc("/update/counter/", UpdateCounterMetric)
 
 	// запуск сервера с адресом localhost, порт 8080
 	http.ListenAndServe(":8080", nil)
 }
 
-// Сохранение метрики
-func UpdateMetric(w http.ResponseWriter, r *http.Request) {
+// Сохранение метрики Gauge
+func UpdateGaugeMetric(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("<h1>Hello, World</h1>"))
+	w.Write([]byte("<h1>Gauge metric</h1>"))
+
+}
+
+// Сохранение метрики Counter
+func UpdateCounterMetric(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("<h1>Counter metric</h1>"))
 
 }
